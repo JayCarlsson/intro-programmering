27eff60a32cbdb23f5a9b9c64f1915e599e91152
Jay Carlsson <jay.carlsson06@icloud.com>
2024-09-09 12:23:57 +0200

    tal och frukt

--- 7050 If Statements/u1.py
+++ 7050 If Statements/u1.py
@@ -1,15 +1,21 @@
 import time
 text = input("Ange ett heltal: ")
 num = int(text)
+time.sleep(2)
+
 if num % 2 == 0:
-    time.sleep(2)
     print("Talet är jämnt")
 else:
     print("Talet är inte jämnt"), exit()
+
 time.sleep(2)
 text2 = input("Gissa min favoritfrukt: ")
 frukt = str(text2)
+time.sleep(2)
 
 if frukt == ("Banan"):
-    time.sleep(2)
     print("Du gissa rätt, min favoritfrukt är banan!")
+elif frukt == ("Äpple"):
+    print("Inte riktigt rätt, äpplen är andra plats!")
+else:
+    print(frukt, "är tyvärr inte min favorit!")
