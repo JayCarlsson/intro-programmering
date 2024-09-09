006ff0257d89c7b747d437930be4791f5e3438c9
Jay Carlsson <jay.carlsson06@icloud.com>
2024-09-09 12:12:12 +0200

    if,wait2

--- 7050 If Statements/u1.py
+++ 7050 If Statements/u1.py
@@ -1,8 +1,15 @@
+import time
 text = input("Ange ett heltal: ")
 num = int(text)
 if num % 2 == 0:
+    time.sleep(2)
     print("Talet är jämnt")
 else:
     print("Talet är inte jämnt"), exit()
+time.sleep(2)
+text2 = input("Gissa min favoritfrukt: ")
+frukt = str(text2)
 
-print("Hejdå")
+if frukt == ("Banan"):
+    time.sleep(2)
+    print("Du gissa rätt, min favoritfrukt är banan!")
--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 Jay Carlsson Loggbok
 ====================
+------------
+24-09-09
+------------
+Jag har använt if-satser i olika sammanhang samt wait.sleep.
+------------
 24-09-02
 ------------
 Jag har använt olika variabler och funktioner.
